8db6fe02ed86825d068d39a07253baf7ed4bec5e
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2023-09-06 17:28:00 -0300

    feat: create fastify config

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-import { createServer } from 'node:http'
+// import { createServer } from 'node:http'
 
-const server = createServer((request, response) => {
-  response.write('oi olá')
+// const server = createServer((request, response) => {
+//   response.write('oi olá')
 
-  return response.end()
+//   return response.end()
+// })
+
+// server.listen(3333)
+
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+server.get('/', () => {
+  return 'Hello world'
 })
 
-server.listen(3333)
+server.listen({
+  port: 3333,
+})
